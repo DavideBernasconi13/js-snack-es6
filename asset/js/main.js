6a123b51b4b5ed2cfb921467f320ea9b64fa9bf4
davidebernasconi13 <davideb.bernasconi@gmail.com>
2024-03-22 15:51:27 +0100

    snack 2

--- asset/js/main.js
+++ asset/js/main.js
@@ -22,3 +22,97 @@ tavoloVip.forEach((element, index) => {
 });
 
 console.log(result);
+
+// Id  Name                Grades
+// 213 Marco della Rovere      78
+// 110 Paola Cortellessa       96
+// 250 Andrea Mantegna         48
+// 145 Gaia Borromini          74
+// 196 Luigi Grimaldello       68
+// 102 Piero della Francesca   50
+// 120 Francesca da Polenta    84
+
+const studenti = [
+    {
+        id: 213,
+        name: 'Marco della Rovere',
+        grades: 78
+    },
+    {
+        id: 110,
+        name: 'Paola Cortellessa',
+        grades: 96
+    },
+    {
+        id: 250,
+        name: 'Andrea Mantegna',
+        grades: 48
+    },
+    {
+        id: 145,
+        name: 'Gaia Borromini',
+        grades: 74
+    },
+    {
+        id: 196,
+        name: 'Luigi Grimaldello',
+        grades: 68
+    },
+    {
+        id: 102,
+        name: 'Piero della Francesca',
+        grades: 50
+    },
+    {
+        id: 120,
+        name: 'Francesca da Polenta',
+        grades: 84
+    }
+]
+
+const printUpper = studenti.map((el) => {
+    return el.name.toUpperCase();
+})
+
+let ulEl = document.querySelector('ul');
+
+for (let i = 0; i < printUpper.length; i++) {
+    const liEl = document.createElement('li');
+    liEl.innerHTML = printUpper[i];
+    ulEl.appendChild(liEl);
+}
+
+// console.log(printUpper); 
+
+
+// lista superiori a 70
+const over70 = studenti.filter((el) => {
+    if (el.grades >= 70) {
+        return true;
+    }
+})
+// stampo la lista a schermo
+let over70Print = document.getElementById('over70');
+over70.forEach(el => {
+    const liEl = document.createElement('li');
+    liEl.innerHTML = el.name + ` ha ottenuto <strong>${el.grades}</strong> punti.`
+    over70Print.appendChild(liEl)
+});
+//console.log(over70); 
+
+// lista superiore a 70 e con id maggiore di 120
+
+const overGradeId = studenti.filter((el) => {
+    const idDesiderata = 120;
+    if (el.grades >= 70 && el.id > idDesiderata) {
+        return true;
+    }
+})
+//stampa lista
+let overGradeIdPrint = document.getElementsByTagName('div')[0];
+overGradeId.forEach(el => {
+    const liEl = document.createElement('p');
+    liEl.innerHTML = el.id + ' ' + el.name + ' ' + el.grades;
+    overGradeIdPrint.appendChild(liEl);
+})
+console.log(overGradeId);
